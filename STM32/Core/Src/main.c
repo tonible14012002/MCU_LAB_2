1c059d8f1c1a99ce794626b4d90dbad5b42cf37d
tonible14012002 <namanhble14012002@gmail.com>
2023-10-09 14:06:17 +0700

    feat: ex3

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -106,6 +106,60 @@ void display7SEG(int num){
 			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
 			break;
+	 case 4:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
+			break;
+	 case 5:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
+			break;
+	 case 6:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 0) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
+			break;
+	 case 7:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 1) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 1 ) ;
+			break;
+	 case 8:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 0) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
+			break;
+	 case 9:
+			HAL_GPIO_WritePin ( SEG0_GPIO_Port , SEG0_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG1_GPIO_Port , SEG1_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG2_GPIO_Port , SEG2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG3_GPIO_Port , SEG3_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG4_GPIO_Port , SEG4_Pin , 1) ;
+			HAL_GPIO_WritePin ( SEG5_GPIO_Port , SEG5_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( SEG6_GPIO_Port , SEG6_Pin , 0 ) ;
+			break;
 	 default:
 		 break;
 
@@ -123,12 +177,18 @@ int enableSeg(){
 		 case secondSeg:
 			HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin , 1 ) ;
 			break;
 		 case thirdSeg:
+			HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin , 0 ) ;
+			HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin , 1 ) ;
 			break;
 		 case fourthSeg:
+			HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin , 1 ) ;
+			HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin , 1 ) ;
 			HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin , 0 ) ;
 			break;
@@ -139,36 +199,36 @@ int enableSeg(){
 
 int counter = COUNTER;
 int ledCounter = LED_COUNTER;
-void changeSEG(){
+int led_buffer [4] = {5 , 3 , 2 , 1};
+void update7SEG(){
 	counter--;
 	if(counter <= 0) {
 			switch(state){
 			 case firstSeg:
 				 	enableSeg();
-					display7SEG(1);
+					display7SEG(led_buffer[firstSeg]);
 					state = secondSeg;
 					break;
 			 case secondSeg:
 				 	enableSeg();
-					display7SEG(2);
-					state = thirdSeg;
+				 	display7SEG(led_buffer[secondSeg]);
+				 	state = thirdSeg;
 				 	break;
 			 case thirdSeg:
 				    enableSeg();
-					display7SEG(3);
-					state = fourthSeg;
+				    display7SEG(led_buffer[thirdSeg]);
+				    state = fourthSeg;
 					break;
 			 case fourthSeg:
 				 	enableSeg();
-					display7SEG(0);
-					state = firstSeg;
+				 	display7SEG(led_buffer[fourthSeg]);
+				 	state = firstSeg;
 					break;
 			 default:
 				 	break;
 			}
 			counter = COUNTER;
 	}
-
 }
 
 void blinkTwoLeds(){
@@ -181,8 +241,8 @@ void blinkTwoLeds(){
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	changeSEG();
- 	blinkTwoLeds();
+	update7SEG();
+	blinkTwoLeds();
 }
 
 int main(void)
